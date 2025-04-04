c187ee0d4bdc3b7afed5873fe738aaaf0e9c2795
Dennis <Denni@DESKTOP-R46DN25.Speedport_W_724V_Typ_A_05011603_06_003>
2025-04-05 01:49:39 +0200

    fancy corridor and resulution settings

--- holgui2/scenes/corridor/corridor_movement.gd
+++ holgui2/scenes/corridor/corridor_movement.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
 #//delete me
+var speed = .02
 
 func _process(delta: float) -> void:
-	position.x = position.x - .05
+	position.x = position.x - speed
 	
 	if(position.x <0):
 		position.x = 8
