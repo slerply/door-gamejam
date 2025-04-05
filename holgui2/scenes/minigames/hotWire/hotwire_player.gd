e73eba613c5f81e44c96fbb551b8e8128f78cd8c
Dennis <Denni@DESKTOP-R46DN25.Speedport_W_724V_Typ_A_05011603_06_003>
2025-04-05 13:15:08 +0200

    very ugly hot wire, workes but does not have a finish signal

--- holgui2/scenes/minigames/hotWire/hotwire_player.gd
+++ holgui2/scenes/minigames/hotWire/hotwire_player.gd
@@ -3,9 +3,9 @@ extends Button
 
 func _process(delta: float) -> void:
 	if(button_pressed):
-		position =  get_global_mouse_position()
-		position.x -= size.x/2
-		position.y -= size.y/2
+		global_position =  get_global_mouse_position()
+		global_position.x -= size.x/2
+		global_position.y -= size.y/2
 
 
 func _on_win_area_area_entered(area: Area2D) -> void:
@@ -15,6 +15,15 @@ func _on_win_area_area_entered(area: Area2D) -> void:
 
 func _on_death_area_area_entered(area: Area2D) -> void:
 	print("you loose")
-	button_pressed= false
-	position = Vector2(0,0)
+	disabled = true
+	global_position = get_parent().global_position
+	position.x -= size.x/2
+	position.y -= size.y/2
+	$reenableButtonDelay.start()
+	pass # Replace with function body.
+
+
+func _on_reenable_button_delay_timeout() -> void:
+	print("go")
+	disabled=false
 	pass # Replace with function body.
