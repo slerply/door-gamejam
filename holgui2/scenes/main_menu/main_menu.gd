02bfceb02c51e40f350a1055cc6b26c4064ec7ed
AlphaZero999 <63010306+AlphaZero999@users.noreply.github.com>
2025-04-06 21:55:02 +0200

    update menus

--- holgui2/scenes/main_menu/main_menu.gd
+++ holgui2/scenes/main_menu/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+#@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	#animated_sprite_2d.play("default")
+	pass
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/intro_scene/intro_scene.tscn")
