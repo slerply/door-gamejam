c54ee31b1cf0a2f06d7c69d1439e967ac58e7027
AlphaZero999 <63010306+AlphaZero999@users.noreply.github.com>
2025-04-06 20:59:41 +0200

    add sound_slider functionality

--- holgui2/scenes/main_menu/main_menu.gd
+++ holgui2/scenes/main_menu/main_menu.gd
@@ -6,7 +6,7 @@ func _on_play_button_pressed():
 
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/options_menu/options_menu.gd")
+	get_tree().change_scene_to_file("res://scenes/options_menu/options_menu.tscn")
 
 
 func _on_quit_button_pressed():
